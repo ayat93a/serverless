0a0c2a68ce8a4dac37f8ee70c85550b13b4d61b5
Ayat <alkayedayat93@gmail.com>
2022-04-12 02:43:13 +0300

    edit readme'

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # vercel link : 
-`https://serverless-beryl-chi.vercel.app/api/date`
 
-i have a `500` --> This Serverless Function has crashed.
+date file --> `https://serverless-obb51dn4g-ayat93a.vercel.app/api/date`
 
-i found that the problem in vercel --> it didnt git the new update in the github, i think i need a help for that.
+
+
+
+text file --> `https://serverless-obb51dn4g-ayat93a.vercel.app/api/text`
+
+*i have a `404 error` --> i found that the problem in vercel --> it didnt git the new update in the github, i think i need a help for that.*
--- api/date.py
+++ api/date.py
@@ -13,8 +13,10 @@ class handler(BaseHTTPRequestHandler):
     dic = dict(query_string_list)
     
     message = f'\n Greetings from python version {platform.python_version()}'
-    message2 = f'\n here a list of recommended books to learn Python'
-    
+    message2 = f'\n you will find a list of recommended books to learn Python if you fetch api/text link'
+    message3 = f'\n these books are fron python.org'
+    message4 = f'\n this trial serverless function is for lab 16'
+
             
     self.send_response(200)
 
@@ -26,7 +28,8 @@ class handler(BaseHTTPRequestHandler):
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     self.wfile.write(message.encode())
     self.wfile.write(message2.encode())
-   
+    self.wfile.write(message3.encode())
+    self.wfile.write(message4.encode())
     return
 
 
