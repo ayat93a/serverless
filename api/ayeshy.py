6c3ca69bca81d007168afd4eb6c93d514c6ec12a
Ayat <alkayedayat93@gmail.com>
2022-04-12 00:54:51 +0300

    added new file

--- api/ayeshy.py
+++ api/ayeshy.py
@@ -1,8 +1,8 @@
 
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+
 from urllib import parse
-import platform
+
 
 class handler(BaseHTTPRequestHandler):
 
@@ -12,20 +12,13 @@ class handler(BaseHTTPRequestHandler):
     query_string_list = parse.parse_qsl(url_components.query)
     dic = dict(query_string_list)
     
-    message = f'\n Greetings from python version {platform.python_version()}'
-    message2 = f'\n here a list of recommended books to learn Python'
-    
-            
+   
     self.send_response(200)
 
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
     
     
-
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    self.wfile.write(message.encode())
-    self.wfile.write(message2.encode())
     books_path = 'text_file/books.txt'
     content = open(books_path, 'r').read()
     self.wfile.write(content)
